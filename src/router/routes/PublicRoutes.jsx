ae001ff61f4790555cdb03c29ab085e716d475d5
Md Mohibbullah <mohammedhanif74612@gmail.com>
2026-08-27 03:34:07 +0300

    modi: modify route files

--- src/router/routes/PublicRoutes.jsx
+++ src/router/routes/PublicRoutes.jsx
@@ -1,4 +1,6 @@
 import { lazy } from "react";
+import SellerLogin from "../../views/auth/SellerLogin";
+import Unauthorize from "../../views/auth/Unauthorize";
 
 const Login = lazy(() => import("../../views/auth/Login"));
 const Register = lazy(() => import("../../views/auth/Register"));
@@ -17,6 +19,14 @@ const publicRoutes = [
   {
     path: "/admin/login",
     element: <AdminLogin />,
+  },
+  {
+    path: "/seller/login",
+    element: <SellerLogin />,
+  },
+  {
+    path: "/unauthorized",
+    element: <Unauthorize />,
   },
 ];
 
---
+++ src/router/routes/index.js
@@ -0,0 +1,8 @@
+// import all route-components to maintain all type of route-files from one place
+// export them to Router.jsx file from here
+
+export { default as PublicRoutes } from "./PublicRoutes";
+export { default as AdminRoutes } from "./AdminRoutes";
+export { default as SellerRoutes } from "./SellerRoutes";
+export { default as PublicGaurd } from "./PublicGuard";
+export { default as RrotectRoutes } from "./ProtectRoute";

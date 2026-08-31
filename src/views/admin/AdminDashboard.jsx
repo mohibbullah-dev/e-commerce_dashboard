5f82a639750dff7548b795c52bda4dbee56e21bf
Md Mohibbullah <mohammedhanif74612@gmail.com>
2026-08-31 22:04:57 +0300

    feat: add chart design in adminDashboard

--- src/views/admin/AdminDashboard.jsx
+++ src/views/admin/AdminDashboard.jsx
@@ -4,7 +4,54 @@ import { MdProductionQuantityLimits } from "react-icons/md";
 import { MdOutlineShoppingCartCheckout } from "react-icons/md";
 import { FaUsers } from "react-icons/fa";
 import { FaCartPlus } from "react-icons/fa";
+import Chart from "react-apexcharts";
 const AdminDashboard = () => {
+  const state = {
+    series: [
+      { name: "Orders", data: [23, 45, 67, 89, 90, 23, 54, 65, 89, 45] },
+      { name: "Revenue", data: [39, 59, 60, 30, 49, 50, 23, 52, 67, 87, 45] },
+      { name: "Sllers", data: [23, 54, 67, 87, 45, 23, 54, 60, 87, 42] },
+    ],
+    options: {
+      color: ["181ee8", "181ee8"],
+      plotOptionsL: {
+        radius: 30,
+      },
+      chart: {
+        background: "#364153",
+        foreColor: "#D0d4dc",
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      strock: {
+        show: true,
+        curve: ["smooth", "straigth", "stepline"],
+        lineCap: "butt",
+        colors: "#f0f0f0",
+        width: ".5",
+        dashArray: 0,
+      },
+      xaxis: {
+        categories: [
+          "Jan",
+          "Fab",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "July",
+          "Aug",
+          "Sep",
+          "Oct",
+          "Nov",
+          "Dec",
+        ],
+      },
+      legend: { position: "top" },
+    },
+  };
+
   return (
     <div className="p-2 m-2 bg-gray-100 flex flex-col inset-0 rounded-sm">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ">
@@ -53,6 +100,21 @@ const AdminDashboard = () => {
             <FaCartPlus size={30} className="text-white" />
           </div>
         </div>
+      </div>
+
+      <div className=" mt-4 w-full flex">
+        {/* chart */}
+
+        <div className="w-3/5 rounded-md">
+          <Chart
+            options={state.options}
+            series={state.series}
+            type="bar"
+            height={350}
+          />
+        </div>
+
+        <div className="w-2/5 ">fff</div>
       </div>
     </div>
   );

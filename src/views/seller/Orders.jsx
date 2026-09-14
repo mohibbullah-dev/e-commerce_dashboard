2897c813f90446081e7f7377bce0c709ddb12f9d
Md Mohibbullah <mohammedhanif74612@gmail.com>
2026-09-14 12:16:45 +0300

    feat: add Orders page

---
+++ src/views/seller/Orders.jsx
@@ -0,0 +1,128 @@
+import React, { useState } from "react";
+import { IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowUp } from "react-icons/io";
+import { Link } from "react-router-dom";
+import Paginations from "../components/Paginations";
+
+const Orders = () => {
+  const [sub_order_show, setSub_order_show] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalItems = 2000;
+  const itemPerPage = 10;
+
+  return (
+    <div className="p-2 m-2 bg-gray-100 flex flex-col inset-0 rounded-sm">
+      <div className="w-full p-4 bg-gray-500 rounded-md">
+        <div className="flex justify-between items-center">
+          <select
+            onChange={(e) => setParPage(parseInt(e.target.value))}
+            className="px-4 py-2 hover:border-green-600 outline-none bg-transparent border border-gray-400 rounded-md text-[#d0d2d6]"
+          >
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="20">20</option>
+          </select>
+          <input
+            className="px-4 py-2 focus:border-green-600 outline-none bg-transparent border border-gray-400 rounded-md text-[#d0d2d6]"
+            type="text"
+            placeholder="search"
+          />
+        </div>
+
+        <div className="relative mt-5 overflow-x-auto  px-2">
+          <div className="w-full text-sm text-left text-[#d0d2d6] bg-gray-600/40 rounded-md px-2">
+            <div className="text-sm text-[#d0d2d6] uppercase border-b-1 border-slate-700">
+              <div className="flex justify-between items-center">
+                <div className="py-3 w-[25%] font-bold">Order id</div>
+                <div className="py-3 w-[13%] font-bold">Price</div>
+                <div className="py-3 w-[18%] font-bold">Payment Status</div>
+                <div className="py-3 w-[18%] font-bold">Order Status</div>
+                <div className="py-3 w-[18%] font-bold">Action </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="text-[#d0d2d6] px-2">
+            <div
+              className={`flex justify-between items-start border-b px-1 border-slate-600/70`}
+            >
+              <div className="py-3 w-[25%] font-medium whitespace-nowrap">
+                #34343
+              </div>
+              <div className="py-3 w-[13%] font-medium">$654</div>
+              <div className="py-3 w-[18%] font-medium">Pending</div>
+              <div className="py-3 w-[18%] font-medium">Pending</div>
+              <div className="py-3 w-[18%] font-medium">
+                <Link to="/admin/order/detail/2">View</Link>
+              </div>
+            </div>
+
+            {[1, 2, 3, 4].map((item, i) => (
+              <div
+                key={i}
+                className={`flex justify-between items-start bg-gray-400/70 border-b px-1 border-slate-600/70 ${sub_order_show ? "flex" : "hidden"} transition-transform duration-500`}
+              >
+                <div className=" w-[25%] font-medium whitespace-nowrap">
+                  #34343
+                </div>
+                <div className="py-3 w-[13%] font-medium">$654</div>
+                <div className="py-3 w-[18%] font-medium">Pending</div>
+                <div className="py-3 w-[18%] font-medium">Pending</div>
+                <div className="py-3 w-[18%] font-medium">
+                  <Link>View</Link>
+                </div>
+                <div className="py-3 w-[8%] font-medium cursor-pointer">
+                  {sub_order_show ? <IoIosArrowDown /> : <IoIosArrowUp />}
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div className="text-[#d0d2d6] px-2">
+            <div
+              className={`flex justify-between items-start border-b px-1 border-slate-600/70`}
+            >
+              <div className="py-3 w-[25%] font-medium whitespace-nowrap">
+                #34343
+              </div>
+              <div className="py-3 w-[13%] font-medium">$654</div>
+              <div className="py-3 w-[18%] font-medium">Pending</div>
+              <div className="py-3 w-[18%] font-medium">Pending</div>
+              <div className="py-3 w-[18%] font-medium">
+                <Link>View</Link>
+              </div>
+            </div>
+
+            {[1, 2, 3, 4].map((item, i) => (
+              <div
+                key={i}
+                className={`flex justify-between items-start bg-gray-400/70 border-b px-1 border-slate-600/70 ${sub_order_show ? "flex" : "hidden"} transition-transform duration-500`}
+              >
+                <div className="py-3 w-[25%] font-medium whitespace-nowrap">
+                  #34343
+                </div>
+                <div className="py-3 w-[13%] font-medium">$654</div>
+                <div className="py-3 w-[18%] font-medium">Pending</div>
+                <div className="py-3 w-[18%] font-medium">Pending</div>
+                <div className="py-3 w-[18%] font-medium cursor-pointer">
+                  <Link to="/admin/order/detail/2">hello</Link>
+                </div>
+                <div className="py-3 w-[8%] font-medium cursor-pointer">
+                  {sub_order_show ? <IoIosArrowDown /> : <IoIosArrowUp />}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <Paginations
+          currentPage={currentPage}
+          onPageChange={setCurrentPage}
+          totalItems={totalItems}
+          itemPerPage={itemPerPage}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Orders;
